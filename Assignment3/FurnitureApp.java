7b627638901cac0fc71de907324097486a39794c
dhanasekardsb25 <71974501+dhanasekardsb25@users.noreply.github.com>
2020-10-20 14:57:39 +0530

    assignment3update addtion

--- Assignment3/FurnitureApp.java
+++ Assignment3/FurnitureApp.java
@@ -1,5 +1,6 @@
 import in.dhanasekar.data.*; //check this package for unqualified class names.
 import com.javapeople.data.Tree;
+import in.dhanasekar.furnitures.bedroom.Bed;
 
 class FurnitureApp{
     public static void main(String []args){
@@ -16,6 +17,21 @@ class FurnitureApp{
 			
 			in.dhanasekar.data.Table dttable = new in.dhanasekar.data.Table();
 			System.out.println(dttable); 
+			
+			Bed bed = new Bed();
+			System.out.println(bed);
+			
+			in.dhanasekar.furnitures.livingroom.Chair lvchair = new in.dhanasekar.furnitures.livingroom.Chair();
+			System.out.println(lvchair);
+			
+			in.dhanasekar.furnitures.livingroom.Table lvtable = new in.dhanasekar.furnitures.livingroom.Table();
+			System.out.println(lvtable);
+			
+			in.dhanasekar.furnitures.office.Chair ofchair = new in.dhanasekar.furnitures.office.Chair();
+			System.out.println(ofchair);
+			
+			in.dhanasekar.furnitures.office.Table oftable = new in.dhanasekar.furnitures.office.Table();
+			System.out.println(oftable);
 		
       
     }
